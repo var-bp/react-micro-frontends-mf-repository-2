779ee3cc81ab456065d869dba066071627977f92
Bohdan Polataiko <bohdan.polataiko@gmail.com>
2021-06-30 07:56:09 +0300

    add new federated module

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -86,6 +86,9 @@ module.exports = {
       exposes: {
         './app': path.join(__dirname, '../src/bootstrap.tsx'),
       },
+      remotes: {
+        [env.MICROFRONTEND_REMOTE_3_NAME]: `${env.MICROFRONTEND_REMOTE_3_NAME}@${env.MICROFRONTEND_REMOTE_3_URL}`,
+      },
       shared: getSharedModules(PACKAGE_JSON.dependencies),
     }),
     // A linter for CSS-like syntaxes like SCSS, Sass, Less and SugarSS
